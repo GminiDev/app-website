17c76acf0ac1b77ab41bb146fec9c4cba3ea4a91
suphakorn <gan.suphakorn17@gmail.com>
2021-03-04 20:51:39 +0700

    Security Section

--- index.js
+++ index.js
@@ -5,8 +5,38 @@ $(document).ready(function(){
     $('#tech .owl-carousel').owlCarousel({
         margin:40,
         dots:false,
-        nav:true
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
     });
 
+    //Security owl carousel
+    $('#security .owl-carousel').owlCarousel({
+        margin:40,
+        dots:false,
+        loop:true,
+        nav:true,
+        navText:["<span><i class='fas fa-arrow-left'></i>Previous</span>","<span>Next<i class='fas fa-arrow-right'></i></span>"],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
 });
 
